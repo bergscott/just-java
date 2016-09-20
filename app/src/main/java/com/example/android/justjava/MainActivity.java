a59d8a1b45fd671490c965b120fd3cc85bb9e9c8
bergscott <berg.scott@ymail.com>
2016-09-20 11:43:23 -0500

    add quantity changing buttons and methods

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -22,8 +22,19 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        display(1 + 1);
-        displayPrice(2 * 3);
+        int quantity = 3;
+        display(quantity);
+        displayPrice(quantity * 3);
+    }
+
+    public void increment(View view) {
+        int quantity = 3;
+        display(quantity);
+    }
+
+    public void decrement(View view) {
+        int quantity = 1;
+        display(quantity);
     }
 
     /**
