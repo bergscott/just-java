dab1ceaf03e6c343926613fda19e7102e1ec78ad
bergscott <berg.scott@ymail.com>
2016-09-23 10:32:16 -0500

    add whipped cream box and make scrollable

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.android.justjava;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 import java.text.NumberFormat;
@@ -40,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
      */
     public void submitOrder(View view) {
         int price = calculatePrice();
-        displayMessage(createOrderSummary(price));
+        displayMessage(createOrderSummary(price, hasWhippedCream()));
     }
 
     /**
@@ -56,14 +57,26 @@ public class MainActivity extends AppCompatActivity {
      * Creates a message that summarizes the order
      *
      * @param totalPrice total price of the order
+     * @param addWhippedCream true if whipped cream box was checked
      * @return message that summarizes the order
      */
-    private String createOrderSummary(int totalPrice) {
+    private String createOrderSummary(int totalPrice, boolean addWhippedCream) {
         String summary = "Name: Sailor Scott";
+        summary += "\nAdd whipped cream? " + addWhippedCream;
         summary += "\nQuantity: " + quantity;
         summary += "\nTotal: $" + totalPrice;
         summary += "\nThank you!";
         return summary;
+    }
+
+    /**
+     * Returns true if whipped cream check box is checked, otherwise returns false
+     *
+     * @return state of whipped cream check box
+     */
+    private boolean hasWhippedCream() {
+        CheckBox whippedCreamCheckBox = (CheckBox) findViewById(R.id.whipped_cream_check_box);
+        return whippedCreamCheckBox.isChecked();
     }
 
     /**
